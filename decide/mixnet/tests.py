a47a956090853e371d95a878a9c0ff10185ce03e
ajramirez <ajramirez@us.es>
2021-10-19 12:57:27 +0200

    Update tests.py

--- decide/mixnet/tests.py
+++ decide/mixnet/tests.py
@@ -156,7 +156,7 @@ class MixnetCase(APITestCase):
         self.assertNotEqual(clear, clear2)
         self.assertEqual(sorted(clear), sorted(clear2))
        
-    def test_multiple_auths_mock(self):
+    def _test_multiple_auths_mock(self):
         '''
         This test emulates a two authorities shuffle and decryption.
         '''
